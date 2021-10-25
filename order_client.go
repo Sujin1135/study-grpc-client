cf0619b17d9653069dbd1a2b324f5f3dbf7725b6
chris <mchead1@naver.com>
2021-10-25 22:55:50 +0900

    :sparkles: update test for testing deadline

--- order_client.go
+++ order_client.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
 	"io"
 	"log"
@@ -39,50 +38,51 @@ func main() {
 	defer conn.Close()
 
 	c := pb.NewOrderManagementClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	deadline := time.Now().Add(time.Duration(2 * time.Second))
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
 	defer cancel()
 
 	// search example as below
-	searchStream, _ := c.GetOrders(ctx, &wrappers.StringValue{Value: "Google"})
+	//searchStream, _ := c.GetOrders(ctx, &wrappers.StringValue{Value: "Google"})
+	//
+	//for {
+	//	searchOrder, err := searchStream.Recv()
+	//	if err == io.EOF {
+	//		break
+	//	}
+	//
+	//	log.Printf("Search Result: ", searchOrder)
+	//}
 
-	for {
-		searchOrder, err := searchStream.Recv()
-		if err == io.EOF {
-			break
-		}
+	// deadline example
+	// start an update example as below
+	updateStream, err := c.UpdateOrders(ctx)
 
-		log.Printf("Search Result: ", searchOrder)
+	if err != nil {
+		log.Fatalf("%v.UpdateOrders(_) = _, %v", c, err)
 	}
 
-	// update example as below
-	//// start an update example as below
-	//updateStream, err := c.UpdateOrders(ctx)
-	//
-	//if err != nil {
-	//	log.Fatalf("%v.UpdateOrders(_) = _, %v", c, err)
-	//}
-	//
-	//updOrder1 := pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Google Pixel Book"}, Destination: "Mountain View, CA", Price: 1100.00}
-	//updOrder2 := pb.Order{Id: "103", Items: []string{"Apple Watch S4", "Mac Book Pro", "iPad Pro"}, Destination: "San Jose, CA", Price: 2800.00}
-	//updOrder3 := pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub", "iPad Mini"}, Destination: "Mountain View, CA", Price: 2200.00}
-	//
-	//if err := updateStream.Send(&updOrder1); err != nil {
-	//	log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder1, err)
-	//}
-	//
-	//if err := updateStream.Send(&updOrder2); err != nil {
-	//	log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder2, err)
-	//}
-	//
-	//if err := updateStream.Send(&updOrder3); err != nil {
-	//	log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder3, err)
-	//}
-	//
-	//updateRes, err := updateStream.CloseAndRecv()
-	//if err != nil {
-	//	log.Fatalf("%v.CloseAndRecv() got error %v, want %v", updateStream, err, nil)
-	//}
-	//log.Printf("Update Orders Res : %s", updateRes)
+	updOrder1 := pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Google Pixel Book"}, Destination: "Mountain View, CA", Price: 1100.00}
+	updOrder2 := pb.Order{Id: "103", Items: []string{"Apple Watch S4", "Mac Book Pro", "iPad Pro"}, Destination: "San Jose, CA", Price: 2800.00}
+	updOrder3 := pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub", "iPad Mini"}, Destination: "Mountain View, CA", Price: 2200.00}
+
+	if err := updateStream.Send(&updOrder1); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder1, err)
+	}
+
+	if err := updateStream.Send(&updOrder2); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder2, err)
+	}
+
+	if err := updateStream.Send(&updOrder3); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder3, err)
+	}
+
+	updateRes, err := updateStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got error %v, want %v", updateStream, err, nil)
+	}
+	log.Printf("Update Orders Res : %s", updateRes)
 
 	// process order example as below
 	//streamProcOrder, _ := c.ProcessOrders(ctx)
